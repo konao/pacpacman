7632072c27950b4a6be9c57e0fca1c05e9038a78
Takuya Sekikawa <konao235@gmail.com>
2022-05-02 21:23:51 +0900

    electronのバージョンアップで動かなくなったので微修正(main.jsのcreateWindow部分）

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 let win;
 function createWindow() {
-    win = new BrowserWindow({width: 1480, height: 1024});
+    win = new BrowserWindow({
+        width: 1480,
+        height: 1024,
+        useContentSize: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
     win.loadURL(`file://${__dirname}/index.html`);
     win.on("closed", ()=>{win=null;});
 }
